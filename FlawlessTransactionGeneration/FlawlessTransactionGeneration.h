5a7d97b7acf850e91f5b28e14263f0eb25c0172e
SwiftOverflow <swiftoverflow@yandex.ru>
2019-11-28 14:53:20 +0300

    first installation project

---
+++ FlawlessTransactionGeneration/FlawlessTransactionGeneration.h
@@ -0,0 +1,19 @@
+//
+//  FlawlessTransactionGeneration.h
+//  FlawlessTransactionGeneration
+//
+//  Created by OMELCHUK Daniil on 28/11/2019.
+//  Copyright © 2019 OMELCHUK Daniil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlawlessTransactionGeneration.
+FOUNDATION_EXPORT double FlawlessTransactionGenerationVersionNumber;
+
+//! Project version string for FlawlessTransactionGeneration.
+FOUNDATION_EXPORT const unsigned char FlawlessTransactionGenerationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlawlessTransactionGeneration/PublicHeader.h>
+
+
